d593b7adbb11c50c6504f0d04a7ae7d79ffd9259
sgodis <357770466@qq.com>
2015-12-03 09:06:52 +0800

    problem 10

--- 10.php
+++ 10.php
@@ -5,22 +5,18 @@
  * date: 2015/11/26
  * Time: 17:04
  */
-//unfinish
+//finish
 
 $result = [2, 3, 5];
-for($i = 7; $i < 10; $i = $i + 2){
+$result = 10;
+for($i = 7; $i < 2000000; $i = $i + 2){
     if ($i % 5 == 0) continue;
-    for($j = 3; $j < $i; $j = $j + 2){
+    $max = intval(sqrt($i));
+    for($j = 3; $j <= $max; $j = $j + 2){ //此处应小于等于max，一个数整除它的平发根也不是质数
         if($i % $j == 0){
             continue 2;
         }
     }
-    $result[] = $i;
+    $result = bcadd($result, $i);
 }
-echo array_sum($result);
-
-$sum = 0;
-$arr = range(1, 200000);
-foreach($arr as $val){
-
-}
+echo $result;
